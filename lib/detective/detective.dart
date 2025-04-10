112f2779b369b18568041ffc865fbf988a1fead5
Micha <wintermihailushka@gmail.com>
2025-04-10 21:44:57 +0200

    detective project

--- lib/detective/detective.dart
+++ lib/detective/detective.dart
@@ -1,22 +1,45 @@
 import 'dart:core';
 import 'dart:io';
 
+const int numberDenotingTheTopNumberOfLongestWords = 5;
+
 void main() {
+  int countSymbols = 0;
+  int numberOfCharactersInTheFile = 0;
+  int numberOfUniqueWords = 0;
+  String mostFrequentlyRepeatedWord = '';
+  List<String> topFifeLongestWords = [];
+  List<String> phoneNumbersList = [];
+  List<String> suspiciousNumbers = [];
   int? encryptionKey;
   File secretData = File('lib/detective/intelligence_data.txt');
+  File encryptedData = File('lib/detective/encrypted_intelligence_data.txt');
+  File decipheredData = File('lib/detective/deciphered_intelligence_data.txt');
+  File investigationReport = File('lib/detective/investigation_report.txt.');
+  File encryptedInvestigationReport =
+      File('lib/detective/encrypted_investigation_report.txt');
 
+  String intelligenceData = secretData.readAsStringSync();
+  String investigationReportData = '';
+
+  if (intelligenceData.isEmpty) {
+    print('We have no data :)');
+    exit(0);
+  }
   print(
       "Bandit: Here, this is the stolen data, John, encrypt it, and make sure it doesn't disappear anywhere");
   print('');
-  print('********************************************************************************************************************');
-  String intelligenceData = secretData.readAsStringSync();
+  print(
+      '********************************************************************************************************************');
   print(intelligenceData);
-  int countSymbols = intelligenceData.length;
-  print('********************************************************************************************************************');
+  countSymbols = intelligenceData.length;
+  print(
+      '********************************************************************************************************************');
 
   encryptionKey = getTheKeyFromTheUser(encryptionKey);
 
-  File encryptedData = dataEncodingByKey(countSymbols, encryptionKey, intelligenceData);
+  encryptedData = dataEncodingByKey(
+      countSymbols, encryptionKey, intelligenceData, encryptedData);
   String encryptedIntelligenceData = encryptedData.readAsStringSync();
   countSymbols = encryptedIntelligenceData.length;
   print('');
@@ -25,24 +48,247 @@ void main() {
   print('');
   print('****The encrypted data reaches the detective****');
   print('');
-  
+
   print('Detective: I received encrypted data, where can I get the key?');
   print('Police Officer: Key is: ' '$encryptionKey');
   print('Detective: Thank you officer');
   print("Detective: I'll try to decipher the data");
   print('');
-  print('********************************************************************************************************************');
-  File decipheredData = dataDecipheringByKey(countSymbols, encryptionKey, encryptedIntelligenceData);
+  print(
+      '********************************************************************************************************************');
+  decipheredData = dataDecipheringByKey(
+      countSymbols, encryptionKey, encryptedIntelligenceData, decipheredData);
   String decipheredIntelligenceData = decipheredData.readAsStringSync();
   print(decipheredIntelligenceData);
-  print('********************************************************************************************************************');
+  print(
+      '********************************************************************************************************************');
   print('');
-  print("We got it, the data is decrypted, I'm starting the analysis.");
+  print("I got it, the data is decrypted, I'm starting the analysis.");
+  print('');
+
+  numberOfCharactersInTheFile = countTheNumberOfCharactersInTheText(
+      numberOfCharactersInTheFile, decipheredIntelligenceData);
+  print('Number of characters in the file: $numberOfCharactersInTheFile');
+  // Самый длинный - самый короткий файл по колличеству символов
+
+  numberOfUniqueWords =
+      countsTheNumberOfUniqueWords(decipheredIntelligenceData);
+  print('Number of unique words: $numberOfUniqueWords');
+  // кількість унікальних слів у кожному файлі.
+
+  mostFrequentlyRepeatedWord =
+      findingTheWordThatOccursMostFrequently(decipheredIntelligenceData);
+  print('The most frequently repeated word: $mostFrequentlyRepeatedWord');
+  // слово, которое встречается чаще всего
+
+  topFifeLongestWords = topFifeTheLongestWords(decipheredIntelligenceData);
+  print('');
+  // Знайти топ-5 найдовших слів.
+
+  phoneNumbersList = findingAllPhoneNumbers(decipheredIntelligenceData);
+  // находим все номера
+  print('');
+  suspiciousNumbers = findSuspiciousNumbers(phoneNumbersList);
+  // работа с подозрительными номерами
+  recordingOfTheInvestigationReport(
+      investigationReport,
+      numberOfCharactersInTheFile,
+      numberOfUniqueWords,
+      mostFrequentlyRepeatedWord,
+      topFifeLongestWords,
+      phoneNumbersList,
+      suspiciousNumbers);
+  investigationReportData = investigationReport.readAsStringSync();
+  // записываем данные в новый файл
+  print('');
+  print(
+      'Detective: I have collected the necessary information, now I encrypt the data.');
+  encryptionKey = getTheKeyFromTheUser(encryptionKey);
+  print('Detective: I got the key now I will encrypt it.');
+  print('');
+  encryptedInvestigationReport = dataEncodingByKey(
+      countSymbols, encryptionKey, investigationReportData, encryptedData);
+  print('');
+  print('Detective: Mission accomplished sir.');
+  // зашифровка данных детективом
+
+  cleaningUpFiles(encryptedData, decipheredData, investigationReport, encryptedInvestigationReport);
+}
+
+void cleaningUpFiles(File encryptedData, File decipheredData, File investigationReport, File encryptedInvestigationReport) {
+   encryptedData.writeAsStringSync('');
+  decipheredData.writeAsStringSync('');
+  investigationReport.writeAsStringSync('');
+  encryptedInvestigationReport.writeAsStringSync('');
+}
+
+void recordingOfTheInvestigationReport(
+    File investigationReport,
+    int numberOfCharactersInTheFile,
+    int numberOfUniqueWords,
+    String mostFrequentlyRepeatedWord,
+    List<String> topFifeLongestWords,
+    List<String> phoneNumbersList,
+    List<String> suspiciousNumbers) {
+  investigationReport.writeAsStringSync(
+      'Number of characters in the file: $numberOfCharactersInTheFile; ',
+      mode: FileMode.append);
+  print('');
+  investigationReport.writeAsStringSync(
+      'Number of unique words: $numberOfUniqueWords; ',
+      mode: FileMode.append);
+  print('');
+
+  investigationReport.writeAsStringSync(
+      'The most frequently repeated word: $mostFrequentlyRepeatedWord; ',
+      mode: FileMode.append);
+  print('');
+  investigationReport.writeAsStringSync('Top fife longest words: ',
+      mode: FileMode.append);
+  for (int i = 0; i < topFifeLongestWords.length; i++) {
+    investigationReport.writeAsStringSync('${topFifeLongestWords[i]}, ',
+        mode: FileMode.append);
+  }
+  investigationReport.writeAsStringSync(';', mode: FileMode.append);
+  print('');
+  investigationReport.writeAsStringSync('All phone numbers: ',
+      mode: FileMode.append);
+  for (int i = 0; i < phoneNumbersList.length; i++) {
+    investigationReport.writeAsStringSync('${phoneNumbersList[i]}, ',
+        mode: FileMode.append);
+  }
+  investigationReport.writeAsStringSync(';', mode: FileMode.append);
+  print('');
+  investigationReport.writeAsStringSync('All suspicious numbers: ',
+      mode: FileMode.append);
+  for (int i = 0; i < suspiciousNumbers.length; i++) {
+    investigationReport.writeAsStringSync('${suspiciousNumbers[i]}, ',
+        mode: FileMode.append);
+  }
+  investigationReport.writeAsStringSync(';', mode: FileMode.append);
+}
+
+List<String> findSuspiciousNumbers(List<String> phoneNumbers) {
+  List<String> suspiciousNumbers = [];
+  print('All suspicious numbers: ');
+  if (phoneNumbers.isEmpty) {
+    print('There are no suspicious mobile numbers here.');
+    return [];
+  }
+  for (var phoneNumber in phoneNumbers) {
+    if (phoneNumber.startsWith('+380')) {
+      String phoneDigits = phoneNumber.substring(4);
+      if (phoneDigits.substring(0, 3) ==
+          phoneDigits.substring(phoneDigits.length - 3)) {
+        suspiciousNumbers.add(phoneNumber);
+        continue;
+      }
+      bool isCrossDigits = true;
+      for (int i = 0; i < phoneDigits.length ~/ 2; i++) {
+        if (phoneDigits[i] != phoneDigits[phoneDigits.length - i - 1]) {
+          isCrossDigits = false;
+          break;
+        }
+      }
+      if (isCrossDigits) {
+        suspiciousNumbers.add(phoneNumber);
+      }
+    }
+  }
+  for (int i = 0; i < suspiciousNumbers.length; i++) {
+    print(suspiciousNumbers[i]);
+  }
+  return suspiciousNumbers;
+}
+
+List<String> findingAllPhoneNumbers(String decipheredIntelligenceData) {
+  RegExp phoneRegExp = RegExp(r'\+\d{9,15}');
+  Iterable<RegExpMatch> matches =
+      phoneRegExp.allMatches(decipheredIntelligenceData);
+  List<String> phoneNumbersList = [];
+  print('All phone numbers: ');
+  if (matches.isEmpty) {
+    print('There are no mobile numbers here.');
+    return [];
+  }
+  for (var match in matches) {
+    phoneNumbersList.add(match.group(0)!);
+    print(match.group(0));
+  }
+  return phoneNumbersList;
+}
+
+List<String> topFifeTheLongestWords(String decipheredIntelligenceData) {
+  List<String> intelligenceDataList = decipheredIntelligenceData
+      .split(RegExp(r'\s+')) // разбивает данные в нашей строке по пробелам
+      .map((word) =>
+          word.replaceAll(RegExp(r'\W'), '')) // убирает знаки перепинания
+      .where((word) => RegExp(r'^[a-zA-Z]+$')
+          .hasMatch(word)) // читает только латинские буквы
+      .toList();
+
+  List<String> topFifeLongestWords = [];
+  for (int i = 0; i < numberDenotingTheTopNumberOfLongestWords; i++) {
+    String longestWord = intelligenceDataList.reduce((a, b) {
+      return a.length > b.length ? a : b;
+    });
+    topFifeLongestWords.add(longestWord);
+    intelligenceDataList.remove(longestWord);
+  }
+  print('Top fife longest words: ');
+  for (int i = 0; i < topFifeLongestWords.length; i++) {
+    print(topFifeLongestWords[i]);
+  }
+  return topFifeLongestWords;
+}
+
+String findingTheWordThatOccursMostFrequently(
+    String decipheredIntelligenceData) {
+  List<String> intelligenceDataList =
+      decipheredIntelligenceData.split(RegExp(r'\s+'));
+  var count = <String, int>{};
+  for (final w in intelligenceDataList) {
+    count[w] = 1 + (count[w] ?? 0);
+  }
+  var orderedList = count.keys.toList();
+  orderedList.sort((a, b) => count[b]!.compareTo(count[a]!));
+  return orderedList[0];
+}
+
+int countsTheNumberOfUniqueWords(String decipheredIntelligenceData) {
+  List<String> intelligenceDataList = decipheredIntelligenceData
+      .split(RegExp(r'\s+'))
+      .map((word) => word.replaceAll(RegExp(r'\W'), ''))
+      .where((word) => RegExp(r'^[a-zA-Z]+$').hasMatch(word))
+      .toList();
+  String listElement = '';
+  int numberOfUniqueWords = 0;
+
+  for (int i = 0; i < intelligenceDataList.length; i++) {
+    int numberOfMatches = 0;
+    listElement = intelligenceDataList[i];
+    for (int i = 0; i < intelligenceDataList.length; i++) {
+      if (listElement == intelligenceDataList[i]) {
+        numberOfMatches++;
+      }
+    }
+    if (numberOfMatches == 1) {
+      numberOfUniqueWords++;
+    }
+  }
+  return numberOfUniqueWords;
+}
+
+int countTheNumberOfCharactersInTheText(
+    int numberOfCharactersInTheFile, String decipheredIntelligenceData) {
+  numberOfCharactersInTheFile =
+      decipheredIntelligenceData.length; // длина файла
+  return numberOfCharactersInTheFile;
 }
 
 int getTheKeyFromTheUser(int? encryptionKey) {
   while (encryptionKey == null) {
-    print('Bandit: Enter the encryption key, it must be a number:');
+    print('Enter the encryption key, it must be a number:');
     String? input = stdin.readLineSync();
     encryptionKey = int.tryParse(input ?? '');
     if (encryptionKey == null) {
@@ -52,19 +298,20 @@ int getTheKeyFromTheUser(int? encryptionKey) {
   return encryptionKey;
 }
 
-File dataEncodingByKey(int countSymbols, int key, String data) {
-  File encryptedData = File('lib/detective/encrypted_intelligence_data.txt');
+File dataEncodingByKey(
+    int countSymbols, int key, String data, File encryptedData) {
   for (int i = 0; i < countSymbols; i++) {
     int value = data.codeUnitAt(i) + key;
     String char = String.fromCharCode(value);
     encryptedData.writeAsStringSync(char, mode: FileMode.append);
-    stdout.write('$char '); // когда ключ цифра 3, не работает, не выводит столько знаков сколько есть в файле, при других случаях все норм;
+    stdout.write(
+        '$char '); // когда ключ цифра 3, не работает, не выводит столько знаков сколько есть в файле, при других случаях все норм;
   }
   return encryptedData;
 }
 
-File dataDecipheringByKey(int countSymbols, int key, String data) {
-  File decipheredData = File('lib/detective/deciphered_intelligence_data.txt');
+File dataDecipheringByKey(
+    int countSymbols, int key, String data, File decipheredData) {
   for (int i = 0; i < countSymbols; i++) {
     int value = data.codeUnitAt(i) - key;
     String char = String.fromCharCode(value);
@@ -72,4 +319,3 @@ File dataDecipheringByKey(int countSymbols, int key, String data) {
   }
   return decipheredData;
 }
-
